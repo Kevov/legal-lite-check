afc2f47c10407732fd9a44a5ae9aa32ade70cf52
Khoa Luong <khoa.luong@yahoo.com.vn>
2025-07-23 10:18:43 -0700

    gh-pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base:"/legal-lite-check",
+  base:"/",
   server: {
     host: "::",
     port: 8080,
